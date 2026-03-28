1a7bb8eee8e61428b3ec88e813304e2ef6cf5b21
abhishek3256 <trexroy16@gmail.com>
2026-03-29 00:03:42 +0530

    yes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
+let cachedDb = null;
+
 const connectDB = async () => {
+    if (cachedDb) {
+        return cachedDb;
+    }
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
+        cachedDb = conn;
         console.log(`MongoDB Connected: ${conn.connection.host}`);
+        return cachedDb;
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
